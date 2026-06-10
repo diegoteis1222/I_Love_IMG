0885c9d4ac13d7b66e861c254439585550abbb0a
Diegoteis <38132090+diegoteis1222@users.noreply.github.com>
2026-06-11 00:23:01 +0200

    antidocker

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  //output: 'standalone',
   experimental: {
     // Sharp se ejecuta nativo en Node, no transpilar
     serverComponentsExternalPackages: ['sharp']
